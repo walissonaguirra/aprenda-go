ef9618a924731f533c06b8d4ce7d376ee0413da3
Walisson Aguirra <walissonaguirra@proton.me>
2025-10-27 11:23:43 -0300

    Cap. 4 – Fundamentos da Programação – 1. Tipo booleano

---
+++ 04_fundamentos-de-programacao/01_tipo-booleano/main.go
@@ -0,0 +1,20 @@
+/*
+*
+Tipo booleano
+Agora vamos explorar os tipos de maneira mais detalhada. golang.org/ref/spec. A começar pelo bool.
+O tipo bool é um tipo binário, que só pode conter um dos dois valores: true e false. (Verdadeiro ou falso, sim ou não, zero ou um, etc.)
+Sempre que você ver operadores relacionais (==, <=, >=, !=, <, >), o resultado da expressão será um valor booleano.
+Booleans são fundamentais nas tomadas de decisões em lógica condicional, declarações switch, declarações if, fluxo de controle, etc.
+Na prática:
+
+	Zero value
+	Atribuindo um valor
+	Bool como resultado de operadores relacionais
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tipo booleano")
+}
