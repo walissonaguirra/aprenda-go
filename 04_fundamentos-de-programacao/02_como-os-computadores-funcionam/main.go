415b533ddcbfef6b796686f8016febab60c0c5fc
Walisson Aguirra <walissonaguirra@proton.me>
2025-10-27 11:35:08 -0300

    Cap. 4 – Fundamentos da Programação – 2. Como os computadores funcionam

---
+++ 04_fundamentos-de-programacao/02_como-os-computadores-funcionam/main.go
@@ -0,0 +1,17 @@
+/*
+*
+Como os computadores funcionam
+
+	Isso é importante pois daqui pra frente vamos falar de ints, bytes, e etc.
+	Não é necessário um conhecimento a fundo mas é importante ter uma idéia de como as coisas funcionam por trás dos panos.
+	https://docs.google.com/presentation/d/1aVytiGOBVDMISFW-ZARJ5iFY1osU2XJIw0hQpNICXm8/
+	ASCII: https://en.wikipedia.org/wiki/ASCII
+	Filme: Alan Turing, The Immitation Game.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Como os computadores funcionam")
+}
