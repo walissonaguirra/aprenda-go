12c3d32e57546bb01f81f6501a28d37c425968e8
Walisson Aguirra <walissonaguirra@proton.me>
2025-11-01 09:04:51 -0300

    Cap. 9 – Exercícios: Nível #4 – 10

---
+++ 09_exercícios-ninja-nivel-4/10/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	g := map[string][]string{}
+
+	g["walisson_aguirra"] = []string{"Estudar", "Programar", "Caminha"}
+	g["sara_aguirra"] = []string{"Viajar", "Ler"}
+	g["manu_aguirra"] = []string{"Estudar", "Socializar"}
+
+	delete(g, "sara_aguirra")
+	delete(g, "manu_aguirra")
+
+	for key, value := range g {
+		fmt.Println("nome:", key, "hobbies:")
+		for _, h := range value {
+			fmt.Println("-", h)
+		}
+		fmt.Println("")
+	}
+}
