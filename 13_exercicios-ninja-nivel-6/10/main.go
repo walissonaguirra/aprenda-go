24d8a9f42bcc17220dbbc15a202e893e430cf3fb
Walisson Aguirra <walissonaguirra@proton.me>
2025-11-05 11:08:18 -0300

    Cap. 13 – Exercícios: Nível #6 – 10

---
+++ 13_exercicios-ninja-nivel-6/10/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := i()
+	b := i()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func i() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
+}
