be9378f3608ee423510bb1adb65c6a68b039e446
Walisson Aguirra <walissonaguirra@proton.me>
2025-11-08 04:30:50 -0300

    Cap. 16 – Aplicações – 3. JSON unmarshal (desordenação)

---
+++ 16_aplicacoes/03_json-unmarshal-desordenacao/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+
+	jsonBlob := []byte(`[
+		{"Name": "Platypus", "Order": "Monotremata"},
+		{"Name": "Quoll",    "Order": "Dasyuromorphia"}
+	]`)
+
+	type Animal struct {
+		Name  string
+		Order string
+	}
+
+	var animals []Animal
+
+	err := json.Unmarshal(jsonBlob, &animals)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Printf("%+v", animals)
+}
