e1bda41d0bead0280bcf56acc0e251bac6d58033
Walisson Aguirra <walissonaguirra@proton.me>
2025-11-08 04:31:58 -0300

    Cap. 16 – Aplicações – 7. bcrypt

---
+++ 16_aplicacoes/07_bcrypt/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	senha := "20julho1980"
+	senhaerrada := "20julho1990"
+
+	sb, err := bcrypt.GenerateFromPassword([]byte(senha), 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(sb))
+
+	fmt.Println(bcrypt.CompareHashAndPassword(sb, []byte(senha)))
+	fmt.Println(bcrypt.CompareHashAndPassword(sb, []byte(senhaerrada)))
+
+}
